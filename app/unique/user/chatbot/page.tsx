f30cb5bdf66a6f954cb565a5b2fc1ee48c57ce94
manishmp2it <manish.mp2it.mtr@gmail.com>
2023-06-09 11:05:53 +0530

    ok

--- app/unique/user/chatbot/page.tsx
+++ app/unique/user/chatbot/page.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import ClientComponent from './ClientComponent';
-import getChatbotdetails,{IListingsParams} from '@/app/actions/getChatbotdetails';
+import getChatbotdetails, { IListingsParams } from '@/app/actions/getChatbotdetails';
 import getAllChatbotQuestion from '@/app/actions/getAllChatbotQuestion';
+import dynamic from 'next/dynamic';
 
 interface ChatbotProps {
     searchParams: IListingsParams;
-  }
+}
 
-const page = async({searchParams}:ChatbotProps) => {
+const ClientComponent = dynamic(() => import('./ClientComponent'), { 
+    ssr: false 
+  });
+
+const page = async ({ searchParams }: ChatbotProps) => {
 
     const chatbot_detail = await getChatbotdetails(searchParams);
     const chatbot_questions = await getAllChatbotQuestion(searchParams);
-
 
     return (
         <ClientComponent chatbot_detail={chatbot_detail} chatbot_questions={chatbot_questions} />
